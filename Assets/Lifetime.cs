255cbf476596b5252032dbc6938fbe00be14db84
zinkem <zinkem@gmail.com>
2015-03-08 21:57:58 -0700

    Orange player sprite, added lifetime to exhaust sprites

---
+++ Assets/Lifetime.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lifetime : MonoBehaviour {
+  
+  public float lifetime;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+    lifetime--;
+
+    if( lifetime <= 0 )
+      {
+        
+        Destroy(gameObject);
+      }
+
+	}
+}
