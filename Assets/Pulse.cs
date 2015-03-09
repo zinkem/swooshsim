503f8e6656894cde429f9dc315ce57ae53b01ae8
zinkem <zinkem@gmail.com>
2015-03-08 23:12:50 -0700

    Added pulse to exhaust

--- Assets/Pulse.cs
+++ Assets/Pulse.cs
@@ -5,23 +5,25 @@ public class Pulse : MonoBehaviour {
 
 
   public float rate;
+  public float amplitude;
 
   float total;
+  Vector3 refscale;
 
 	// Use this for initialization
 	void Start () {
-	
+    refscale = transform.localScale;
+    total = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
     total += rate;
+    Vector3 theScale = refscale;
 
-    Vector3 theScale = transform.localScale;
-
-    theScale.x += Mathf.Cos(total);
-    theScale.y += Mathf.Sin(total);
+    theScale.x += amplitude*Mathf.Sin(total);
+    theScale.y += amplitude*Mathf.Sin(total);
 
     transform.localScale = theScale;
 
